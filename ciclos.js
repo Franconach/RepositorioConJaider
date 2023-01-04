e01887f37b154ecc24a2b33ebc5ec28300e691b6
JaiderValencia <janvame05@gmail.com>
2023-01-03 22:17:08 -0500

    mis modificaciones

--- ciclos.js
+++ ciclos.js
@@ -1,2 +1,8 @@
 let ciclos //definicion de ciclos
 // realizando cambios en ciclos.js
+function funcionesCiclos(params) {
+    let numeros = [];
+    for (let i = 0; i < 10; i++) {
+        console.log(i)
+    }
+}
